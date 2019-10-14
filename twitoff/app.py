0ce763139e1ac85926c92be4f150c032a644a498
xander-bennett <xander-bennett@users.noreply.github.com>
2019-10-14 13:28:57 -0700

    built models.py

--- twitoff/app.py
+++ twitoff/app.py
@@ -7,6 +7,7 @@ def create_app():
     """create and configures an instance of a flask app"""
     app = Flask(__name__)
 
+    # configuring the DB is done before the routes
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
     DB.init_app(app)
 
