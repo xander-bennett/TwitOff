2d2fcce2de9a91b7f6053ffdf6855afc6f52472d
xander-bennett <xander-bennett@users.noreply.github.com>
2019-10-14 10:01:29 -0700

    adding html about and home files

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,12 +1,17 @@
-from flask import Flask
+### TO RUN THIS: FLASK_APP=twitoff flask run ###
 
+# import flask package. flask makes app objects
+from flask import Flask, render_template
+
+# create Flask web server, makes the application
 def create_app():
     """Create and configure an instance of the flask application."""
     app = Flask(__name__)
 
+# routes determine location
+# Routing to home
     @app.route('/')
-    def root():
-        return 'Hello TwitOff!!'
+    def home():
+        return render_template('home.html')
 
     return app
-
