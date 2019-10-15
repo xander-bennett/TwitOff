30198f9e6896c249db6c7c6911caac892337884c
xander-bennett <xander-bennett@users.noreply.github.com>
2019-10-15 15:43:05 -0700

    added twitter.py and twitter api keys

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,19 +1,31 @@
 """Main application for twitoff"""
 #imports
-from flask import Flask, render_template
-from .models import DB
+from decouple import config
+from flask import Flask, render_template, request
+from .models import DB, User
 
 def create_app():
     """create and configures an instance of a flask app"""
     app = Flask(__name__)
 
     # configuring the DB is done before the routes
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
+    app.config['ENV'] = config('ENV') # should change this later to production
+    app.config['SQLACHEMY_TRACK_MODIFICATIONS'] = False
+
+
     DB.init_app(app)
 
     @app.route('/')
     def root():
-        return render_template('home.html')
+        users = User.query.all()
+        return render_template('base.html', title='Home', users=users)
+
+    @app.route('/reset')
+    def reset():
+        DB.drop_all()
+        DB.create_all()
+        return render_template('base.html', title='DB Reset', users=[])
 
     @app.route("/about")
     def preds():
