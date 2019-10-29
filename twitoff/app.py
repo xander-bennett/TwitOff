b8d6edf3487e9a8ab2112ab4a750e9020863fb58
xander-bennett <xander-bennett@users.noreply.github.com>
2019-10-29 08:35:30 -0700

    commented out modifications tracker

--- twitoff/app.py
+++ twitoff/app.py
@@ -9,7 +9,7 @@ def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
     app.config['FLASK_ENV'] = config('FLASK_ENV')
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     DB.init_app(app)
 
     @app.route('/')
