b226b5491e6c6014028996b0ea004f091874081e
xander-bennett <xander-bennett@users.noreply.github.com>
2019-11-08 13:47:45 -0800

    restarted app from scratch

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,83 +1,65 @@
-"""Main app/routing file for TwitOff."""
-from os import getenv
-from pickle import dumps, loads
-from dotenv import load_dotenv
+from decouple import config
 from flask import Flask, render_template, request
-from .models import DB, User
-from .predict import predict_user
+from .models import DB, User, Tweet
 from .twitter import add_or_update_user, update_all_users
-
-load_dotenv()
-
-if getenv('FLASK_ENV') == 'production':
-    from redis import Redis
-    CACHE = Redis(host=config('REDIS_HOST'), port=config('REDIS_PORT'),
-                  password=config('REDIS_PASSWORD'))
-else:  # development/test, use local mocked Redis
-    from birdisle.redis import Redis
-    CACHE = Redis()
-
-CACHED_COMPARISONS = (loads(CACHE.get('comparisons'))
-                      if CACHE.exists('comparisons') else set())
-
-
+from .predict import predict_user
 def create_app():
-    """Create and configure an instance of the Flask application."""
+    """Create and configure an instance of the Flask Application"""
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URL')
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
+    app.config['ENV'] = config('ENV')
+    app.config['DEBUG'] = config('FLASK_DEBUG')
     DB.init_app(app)
 
     @app.route('/')
     def root():
-        return render_template('base.html', title='Home', users=User.query.all(),
-                               comparisons=CACHED_COMPARISONS)
+        users = User.query.all()
+        return render_template('home.html', title='Welcome to Twitoff!', users=User.query.all())
+
+    @app.route("/about")
+    def preds():
+        return render_template('about.html')
+
+    @app.route("/reset")
+    def reset():
+        DB.drop_all()
+        DB.create_all()
+        return render_template('home.html', title='DB Reset!', users=[])
 
     @app.route('/user', methods=['POST'])
-    @app.route('/user/<name>', methods=['GET'])
+    @app.route('/user/<name>/', methods=['GET'])
     def user(name=None, message=''):
-        name = name or request.values['user_name']
+        name = (name or request.values['user_name'])
         try:
-            if request.method == 'POST':
+            if request.method =='POST':
                 add_or_update_user(name)
-                message = "User {} successfully added!".format(name)
+                message = 'User {} successfully added!'.format(name)
             tweets = User.query.filter(User.name == name).one().tweets
         except Exception as e:
-            message = "Error adding {}: {}".format(name, e)
-            tweets = []
-        return render_template('user.html', title=name, tweets=tweets,
-                               message=message)
+            message = 'Error adding {}: {}'.format(name,e)
+            tweets=[]
+            pass
+        return render_template('user.html', name=name, tweets=tweets, message=message)
+
 
     @app.route('/compare', methods=['POST'])
     def compare(message=''):
-        user1, user2 = sorted([request.values['user1'],
-                               request.values['user2']])
+        user1 = request.values['user1']
+        user2 = request.values['user2']
+        tweet_text = request.values['tweet_text']
+
         if user1 == user2:
             message = 'Cannot compare a user to themselves!'
         else:
-            prediction = predict_user(user1, user2,
-                                      request.values['tweet_text'], CACHE)
-            CACHED_COMPARISONS.add((user1, user2))
-            CACHE.set('comparisons', dumps(CACHED_COMPARISONS))
+            prediction = predict_user(user1, user2, tweet_text)
             message = '"{}" is more likely to be said by {} than {}'.format(
                 request.values['tweet_text'], user1 if prediction else user2,
                 user2 if prediction else user1)
         return render_template('prediction.html', title='Prediction', message=message)
-
-    @app.route('/reset')
-    def reset():
-        CACHE.flushall()
-        CACHED_COMPARISONS.clear()
-        DB.drop_all()
-        DB.create_all()
-        return render_template('base.html', title='Reset database!')
-
+    
     @app.route('/update')
     def update():
-        CACHE.flushall()
-        CACHED_COMPARISONS.clear()
         update_all_users()
-        return render_template('base.html', users=User.query.all(),
-                               title='Cache cleared and all Tweets updated!')
-
+        pass
+        return render_template('home.html', users=User.query.all(), title='All Tweets updated!')
     return app
--- twitoff/twitter.py
+++ twitoff/twitter.py
@@ -1,61 +1,44 @@
-"""Fetch embeddings and tweets for DB"""
-from os import getenv
+"""Retrieve Tweets, embeddings, and persist in the database."""
 import basilica
 import tweepy
+from decouple import config
 from .models import DB, Tweet, User
 
 
-TWITTER_USERS = ['calebhicks', 'elonmusk', 'jtimberlake', 'SteveMartinToGo',
-                 'alyankovic', 'nasa', 'sadserver', 'Pokemon', 'justinbieber',
-                 'common_squirrel', 'KenJennings', 'conanobrien',
-                 'big_ben_clock', 'IAM_SHAKESPEARE', 'playapex', 'taylorswift13',
-                 'wendys', 'realDonaldTrump']
-
-TWITTER_AUTH = tweepy.OAuthHandler(getenv('TWITTER_CONSUMER_KEY'),
-                                   getenv('TWITTER_CONSUMER_SECRET'))
-TWITTER_AUTH.set_access_token(getenv('TWITTER_ACCESS_TOKEN'),
-                              getenv('TWITTER_ACCESS_TOKEN_SECRET'))
+TWITTER_AUTH = tweepy.OAuthHandler(config('TWITTER_CONSUMER_KEY'), config('TWITTER_CONSUMER_SECRET'))
+TWITTER_AUTH.set_access_token(config('TWITTER_ACCESS_TOKEN'), config('TWITTER_ACCESS_TOKEN_SECRET'))
 TWITTER = tweepy.API(TWITTER_AUTH)
-BASILICA = basilica.Connection(getenv('BASILICA_KEY'))
-
+BASILICA = basilica.Connection(config('BASILICA_KEY'))
 
 def add_or_update_user(username):
-    """Add or update a user and their Tweets, error if not a Twitter user."""
+    """Add or Update a user *and* their Tweets, error if no/private user."""
     try:
+        #get user info from tweepy API
         twitter_user = TWITTER.get_user(username)
-        db_user = (User.query.get(twitter_user.id) or
-                   User(id=twitter_user.id, name=username))
+        #do a DB query to see if the twitter user already exists
+        db_user = (User.query.get(twitter_user.id) or User(id=twitter_user.id, name=username))
         DB.session.add(db_user)
-        # We want as many recent non-retweet/reply statuses as we can get
-        # 200 is a Twitter API limit, we'll usually see less due to exclusions
+        #we want as many recent non-retweet/reply statuses as we can get
+        #since_id will pull in the most recent tweet ID we have on file
+        #this allows us to only pull in new tweets.
         tweets = twitter_user.timeline(
-            count=200, exclude_replies=True, include_rts=False,
-            tweet_mode='extended', since_id=db_user.newest_tweet_id)
+            count=200, exclude_replies=True, include_rts=False, tweet_mode='extended',
+            since_id=db_user.newest_tweet_id)
+        #if there are new tweets to pull in, update them here!
+        #this is skipped if there are no new tweets
         if tweets:
             db_user.newest_tweet_id = tweets[0].id
         for tweet in tweets:
-            # Calculate embedding on the full tweet, but truncate for storing
-            embedding = BASILICA.embed_sentence(tweet.full_text,
-                                                model='twitter')
-            db_tweet = Tweet(id=tweet.id, text=tweet.full_text[:300],
-                             embedding=embedding)
+            #Get embedding for tweet and store in DB
+            embedding = BASILICA.embed_sentence(tweet.full_text, model='twitter')
+            db_tweet = Tweet(id=tweet.id, text=tweet.full_text[:500], embedding=embedding)
             db_user.tweets.append(db_tweet)
             DB.session.add(db_tweet)
     except Exception as e:
-        print('Error processing {}: {}'.format(username, e))
+        print('Error processing {}: {}'.format(username,e))
         raise e
     else:
         DB.session.commit()
-
-
-def add_users(users=TWITTER_USERS):
-    """
-    Add/update a list of users (strings of user names).
-    May take awhile, so run "offline" (flask shell).
-    """
-    for user in users:
-        add_or_update_user(user)
-
 
 def update_all_users():
     """Update all Tweets for all Users in the User table."""
